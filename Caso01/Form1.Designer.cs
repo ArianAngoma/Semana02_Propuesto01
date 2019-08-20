0bd80d254dd1597cee33c96178348e7b32ba0ce4
unknown <arian.angoma@tecsup.edu.pe>
2019-08-19 21:49:58 -0500

    first commit

---
+++ Caso01/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Caso01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.DgPedidos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblTotalPedidos = new System.Windows.Forms.Label();
+            this.CboEmpleado = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Apellido y Nombre del Empleado";
+            // 
+            // DgPedidos
+            // 
+            this.DgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgPedidos.Location = new System.Drawing.Point(41, 72);
+            this.DgPedidos.Name = "DgPedidos";
+            this.DgPedidos.Size = new System.Drawing.Size(747, 298);
+            this.DgPedidos.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(642, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Total de Pedidos";
+            // 
+            // LblTotalPedidos
+            // 
+            this.LblTotalPedidos.AutoSize = true;
+            this.LblTotalPedidos.Location = new System.Drawing.Point(735, 390);
+            this.LblTotalPedidos.Name = "LblTotalPedidos";
+            this.LblTotalPedidos.Size = new System.Drawing.Size(46, 13);
+            this.LblTotalPedidos.TabIndex = 6;
+            this.LblTotalPedidos.Text = "             ";
+            // 
+            // CboEmpleado
+            // 
+            this.CboEmpleado.FormattingEnabled = true;
+            this.CboEmpleado.Location = new System.Drawing.Point(218, 26);
+            this.CboEmpleado.Name = "CboEmpleado";
+            this.CboEmpleado.Size = new System.Drawing.Size(236, 21);
+            this.CboEmpleado.TabIndex = 7;
+            this.CboEmpleado.SelectionChangeCommitted += new System.EventHandler(this.CboEmpleado_SelectionChangeCommitted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CboEmpleado);
+            this.Controls.Add(this.LblTotalPedidos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DgPedidos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DgPedidos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblTotalPedidos;
+        private System.Windows.Forms.ComboBox CboEmpleado;
+    }
+}
+
---
+++ Caso01/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Caso01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Caso01"].ConnectionString);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaEmpleados();
+        }
+
+        public void ListaPedidos()
+        {
+            using (SqlDataAdapter df = new SqlDataAdapter("Usp_Listado_Pedidos", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                using (DataSet Da = new DataSet())
+                {
+                    df.Fill(Da, "Ped");
+                    DgPedidos.DataSource = Da.Tables["Ped"];
+                    LblTotalPedidos.Text = Da.Tables["Ped"].Rows.Count.ToString();
+                }
+            }
+        }
+
+        public void ListaEmpleados()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Nombre_Apellidos_Empleado", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    DataTable df = new DataTable();
+                    
+                    da.Fill(df);
+                    CboEmpleado.DataSource = df;
+                    CboEmpleado.DisplayMember = "Empleado";
+                    CboEmpleado.ValueMember = "Empleado";
+                }
+            }
+        }
+
+        private void CboEmpleado_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Listado_Pedidos", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@apellidoNombre", CboEmpleado.SelectedValue);
+
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Completo");
+
+                        DgPedidos.DataSource = df.Tables["Completo"];
+                        LblTotalPedidos.Text = df.Tables["Completo"].Rows.Count.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
